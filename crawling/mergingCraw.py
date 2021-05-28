b0086f17ae68d12b08871866ea5fa9be3f2dcee6
Sophia <cnlwkd@naver.com>
2021-05-28 16:12:45 +0900

    여러 문장으로 구성된 댓글 분해 및 기본 이모지 제거

--- crawling/mergingCraw.py
+++ crawling/mergingCraw.py
@@ -1,4 +1,5 @@
 import os
+import re
 import csv
 
 folder = './DataSets'
@@ -6,6 +7,13 @@ merged = './raw_comments.csv'
 
 os.chdir(folder)
 fileList = os.listdir('.')
+
+emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
 
 with open(merged, 'w', encoding='utf-8', newline='') as dest:
     writer = csv.writer(dest)
@@ -16,4 +24,11 @@ with open(merged, 'w', encoding='utf-8', newline='') as dest:
             for line in reader:
                 if 2 > len(line):
                     continue
-                writer.writerow([line[1].replace('\n', '')])
+
+                line[1] = emoji_pattern.sub(r'', line[1])  # no emoji
+                sLine = re.split('[!.?]', line[1].replace('\n', ''))
+                for wLine in sLine:
+                    if wLine == '':
+                        continue
+                    wLine = wLine.strip()
+                    writer.writerow([wLine])
