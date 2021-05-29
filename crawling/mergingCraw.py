227bad6b9c19be332feaf124f28c75d3781b4de1
Sophia <cnlwkd@naver.com>
2021-05-29 23:00:10 +0900

    크롤링된 데이터 testSet 및 trainSet으로 분리

--- crawling/mergingCraw.py
+++ crawling/mergingCraw.py
@@ -1,34 +1,60 @@
 import os
 import re
 import csv
+import random
 
 folder = './DataSets'
-merged = './raw_comments.csv'
+train = 'trainSet.csv'
+test = 'testSet.csv'
+
+def getRawStrings(train, test):
+    empty = ''
+    fileList = os.listdir('.')
+    charSet = re.compile('[^ 가-힣!.?:0-9]+')
+
+    with open(train, 'w', encoding='utf-8', newline='') as train:
+        with open(test, 'w', encoding='utf-8', newline='') as test:
+            inputRows = []
+            trainData = []
+            testData = []
+            trainWriter = csv.writer(train)
+            testWriter = csv.writer(test)
+
+            for file in fileList:
+                with open(file, 'r', encoding='utf-8') as f:
+                    reader = csv.reader(f)
+                    try:
+                        next(reader)
+                    except StopIteration:
+                        continue
+                    for line in reader:
+                        if 2 > len(line):
+                            continue
+                        line[1] = charSet.sub('', line[1])
+                        line[1] = re.sub('\s+', ' ', line[1])
+                        line[1] = re.sub('[0-9]+:[0-9]+', ' ', line[1]) #유튜브 타임라인  표시
+                        line[1] = re.sub('[0-9]+분[0-9]+초', ' ', line[1]) #유튜브 타임라인  표시
+                        tLine = re.split('[!.?]', line[1].replace('\n', ''))
+                        sLine = [x for x in tLine if x]
+                        for getLine in sLine:
+                            getLine = getLine.strip()
+                            if getLine.count(' ') > 2:
+                                inputRows.append(getLine)
+                            
+            for (i, wLine) in enumerate(inputRows):     #와 소스 진짜 더럽다 ㅋㅋ
+                if i % 2 == 0:
+                    trainData.append(wLine)
+                else:
+                    testData.append(wLine)
+
+            trainData = random.sample(trainData, 20000)
+            testData = random.sample(testData, 5000)
+
+            for element in trainData:
+              trainWriter.writerow([element])
+
+            for element in testData:
+              testWriter.writerow([element])
 
 os.chdir(folder)
-fileList = os.listdir('.')
-
-emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+", flags=re.UNICODE)
-
-with open(merged, 'w', encoding='utf-8', newline='') as dest:
-    writer = csv.writer(dest)
-    for file in fileList:
-        with open(file, 'r', encoding='utf-8') as f:
-            reader = csv.reader(f)
-            next(reader)
-            for line in reader:
-                if 2 > len(line):
-                    continue
-
-                line[1] = emoji_pattern.sub(r'', line[1])  # no emoji
-                sLine = re.split('[!.?]', line[1].replace('\n', ''))
-                for wLine in sLine:
-                    if wLine == '':
-                        continue
-                    wLine = wLine.strip()
-                    writer.writerow([wLine])
+getRawStrings(train, test)
